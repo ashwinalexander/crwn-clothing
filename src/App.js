cb1c3d5bba6dba9f80cd2348c2a4f612a5efde28
ashwin <alexwin75@gmail.com>
2021-01-26 17:19:58 -0500

    adding in the sign in page and related resuable components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React from "react";
 import "./App.css";
 import { Route, Switch } from "react-router-dom";
+import Header from "./components/header/header.component";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 
-function App() {
+const App = () => {
 	return (
 		<div>
+			<Header />
 			<Switch>
 				<Route exact path="/" component={HomePage} />
 				<Route exact path="/shop" component={ShopPage} />
+				<Route exact path="/signin" component={SignInAndSignUpPage} />
 			</Switch>
 		</div>
 	);
-}
+};
 
 export default App;
